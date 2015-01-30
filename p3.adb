1f6f5fc5fd76a3b394fb245ff11bd8970fe5af55
Sam Heather <sam@heather.sh>
2015-01-30 10:56:40 +0000

    Ex 3 works

--- p3.adb
+++ p3.adb
@@ -1,11 +1,36 @@
 with Ada.Text_IO; use Ada.Text_IO;
 procedure P3 is
-	task arrays;
-	task body arrays is
+
+	type Integer_Array is array (0 .. 9) of Integer;
+	oneSeven : Integer_Array := (0,0,0,0,0,0,0,0,0,0);
+	--oneSeven : array (0 .. 100) of Integer;
+	pragma Volatile(oneSeven);
+
+	procedure Writers is
+		task ones;
+		task body ones is
+		begin
+			for i in oneSeven'Range loop
+				oneSeven(i) := 1;
+			end loop;
+		end ones;
+
+		task sevens;
+		task body sevens is
+		begin
+			for i in oneSeven'Range loop
+				oneSeven(i) := 7;
+			end loop;
+		end sevens;
+
 	begin
-	
-	end arrays;
+		null;
+	end;
+
 begin
-	null;
+	Writers;
+	for i in oneSeven'Range loop
+		Put(Integer'Image(oneSeven(i)) & " ");
+	end loop;
 end P3;
 

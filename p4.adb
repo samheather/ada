11b9815ceec48d5bea00d5dcffb1a4f3899baf68
Sam Heather <sam@heather.sh>
2015-01-30 11:02:47 +0000

    Added p4 as original p3 copy

---
+++ p4.adb
@@ -0,0 +1,35 @@
+with Ada.Text_IO; use Ada.Text_IO;
+procedure P3 is
+
+	type Integer_Array is array (0 .. 9) of Integer;
+	oneSeven : Integer_Array := (0,0,0,0,0,0,0,0,0,0);
+	pragma Volatile(oneSeven);
+
+	procedure Writers is
+		task ones;
+		task body ones is
+		begin
+		for i in oneSeven'Range loop
+			oneSeven(i) := 1;
+		end loop;
+		end ones;
+
+		task sevens;
+		task body sevens is
+		begin
+		for i in oneSeven'Range loop
+			oneSeven(i) := 7;
+		end loop;
+		end sevens;
+
+	begin
+		null;
+	end;
+
+begin
+	Writers;
+	for i in oneSeven'Range loop
+		Put(Integer'Image(oneSeven(i)) & " ");
+	end loop;
+end P3;
+

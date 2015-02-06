797714c803f1e38d7da9750bbe7eae38d6bf5ca6
Sam Heather <sam@heather.sh>
2015-02-06 10:29:51 +0000

    Structure of p4, just never stops looping though

--- p4.adb
+++ p4.adb
@@ -1,22 +1,35 @@
 with Ada.Text_IO; use Ada.Text_IO;
 procedure P4 is
 
-	protected type Protected_Integer_Array is
-		entry One;
-		entry Seven;
-	private
-		
-
 	type Integer_Array is array (0 .. 9) of Integer;
 	oneSeven : Integer_Array := (0,0,0,0,0,0,0,0,0,0);
 	pragma Volatile(oneSeven);
+
+	protected Protected_Integer_Array is
+		entry One (index : in Integer);
+		entry Seven (index : in Integer);
+	private
+		countSoFar : Integer := 0;
+		lastWritten : Integer := 7;
+	end Protected_Integer_Array;
+
+	protected body Protected_Integer_Array is
+		entry One (index : in Integer) when lastWritten = 7 is
+		begin
+			oneSeven (index) := 1;
+		end One;
+		entry Seven (index : in Integer) when lastWritten = 1 is
+		begin
+			oneSeven (index) := 7;
+		end Seven;
+	end Protected_Integer_Array;
 
 	procedure Writers is
 		task ones;
 		task body ones is
 		begin
 		for i in oneSeven'Range loop
-			oneSeven(i) := 1;
+			Protected_Integer_Array.One(i);
 		end loop;
 		end ones;
 
@@ -24,7 +37,7 @@ procedure P4 is
 		task body sevens is
 		begin
 		for i in oneSeven'Range loop
-			oneSeven(i) := 7;
+			Protected_Integer_Array.Seven(i);
 		end loop;
 		end sevens;
 

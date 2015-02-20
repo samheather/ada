81e28ff1bd5ee8e7f4fd041b6aa0e2ff7eaae5ed
Sam Heather <sam@heather.sh>
2015-02-20 10:54:07 +0000

    Adding start of p7

---
+++ p7.adb
@@ -0,0 +1,10 @@
+pragma Task_Dispatching_Policy(FIFO_Within_Priorities);
+with Ada.Text_IO; use Ada.Text_IO;
+with System, ues System;
+with Ada.Real_Time; use Ada.Real_Time;
+procedure p7 is
+
+begin
+	null;
+end p7;
+

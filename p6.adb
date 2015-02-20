df999d8d9db11cfe536c9397fb9a46a2b9113a18
Sam Heather <sam@heather.sh>
2015-02-20 10:45:48 +0000

    P6 works, now releasing tasks in Client_ID order with entry families

--- p6.adb
+++ p6.adb
@@ -10,48 +10,48 @@ procedure p6 is
 	-- Client ID's and Individual Barriers
 	type Client_ID is new Integer range 1 .. 5;
 	type Bools is array (Client_ID) of Boolean;
-
-	Releaser : Integer := 3;
 	
 	protected Controller is
-		entry Barrier_Wait(Client : Integer);
+		entry Barrier_Wait(Client : Client_ID);
 	private
-		entry Barrier_Release(Client : Integer);
-		Barrier_Up : Boolean := True;
+		entry Barrier_Release(Client_ID'Range) (Client : Client_ID);
+		Open_Barrier : Bools := (others => False);
+		Arrived : Integer := 0;
 	end Controller;
 	
 	protected body Controller is
-		entry Barrier_Wait(Client : Integer) when True is
+		entry Barrier_Wait(Client : Client_ID) when True is
 		begin
-			if Client = Releaser then
-				Barrier_Up := False;
-			else
-				requeue Barrier_Release with abort;
+			Arrived := Arrived + 1;
+			if Arrived = Integer (Client_ID'Last) then
+				Open_Barrier(Client_ID'First) := True;
 			end if;
+			requeue Barrier_Release(Client) with abort;
 		end Barrier_Wait;
 		
-		entry Barrier_Release(Client : Integer) when Barrier_Up = False is
+		entry Barrier_Release(for I in Client_ID'Range) (Client : Client_ID) when Open_Barrier(I) is
 		begin
-			if Barrier_Release'Count = 0 then
-				Barrier_Up := True;
+			if Client /= Client_ID'Last then
+				Put_Line("Opening barrier: " & Client_ID'Image(Client_ID'Succ(Client)));
+				Open_Barrier (Client_ID'Succ(Client)) := True;
 			end if;
 		end Barrier_Release;
 	end Controller;
 		
-	task type Client(ID : Integer);
+	task type Client(ID : Client_ID);
 	task body Client is
 		Gen : Random_Integer.Generator;
 		Random_Int : Integer;
 	begin
 		Random_Integer.Reset(Gen, Integer(ID));
 		Random_Int := Random_Integer.Random(Gen);
-		delay Randon_Int * 1.0;
-		Put_Line("Task " & Integer'Image(ID) & " at barrier");
+		delay Random_Int * 1.0;
+		Put_Line("Task " & Client_ID'Image(ID) & " at barrier. Had random int: " & Integer'Image(Random_Int));
 		select
 			Controller.Barrier_Wait(ID);
-			Put_Line("Task " & Integer'Image(ID) & " Released");
+			Put_Line("Task " & Client_ID'Image(ID) & " Released");
 		or delay 10.0;
-			Put_Line("Task " & Integer'Image(ID) & " not released");
+			Put_Line("Task " & Client_ID'Image(ID) & " not released");
 		end select;
 	end;
 		

5e93414afa2f4843a7b4d8924b603901de3d28c1
Sam Heather <sam@heather.sh>
2015-02-22 00:35:15 +0000

    Restored back to the real p7, before I mixed it with p8 and p9.

--- p7.adb
+++ p7.adb
@@ -2,31 +2,7 @@ pragma Task_Dispatching_Policy(FIFO_Within_Priorities);
 with Ada.Text_IO; use Ada.Text_IO;
 with System; use System;
 with Ada.Real_Time; use Ada.Real_Time;
-with Ada.Real_Time.Timing_Events; use Ada.Real_Time.Timing_Events;
 procedure p7 is
-	
-	TE : aliased Timing_Event;
-	Period : constant Time_Span := Milliseconds(100);
-
-	Start_Time : Time := Clock + Milliseconds(100);
-
-	protected Releaser is
-		entry Wait_Next_Release;
-		procedure Release(Event : in out Timing_Event);
-	private
-		Go : Boolean := False;
-		Next_Time : Time := Start_Time;
-	end Releaser;
-	
-	protected body Releaser is
-		entry Wait_Next_Release when Go is
-			Go := False;
-		end Wait_Next_Release;
-		
-		procedure Release(Event : in out Timing_Event) is
-			Go := True;
-			Next_Time := Next_Time + Period;
-			Event.Set_Handler(Next_Time, Release'Access); ---- What is this access parameter?
 
 	task Periodic is
 		pragma Priority(System.Priority'First + 5);
@@ -36,7 +12,6 @@ procedure p7 is
 		Period : constant Time_Span := Milliseconds(100);
 		Next_Time : Time;
 	begin
-		delay until Start_Time;
 		for I in Integer range 1 .. 10 loop
 			Next_Time := Clock + Period;
 			Put_Line("High priority task running");
@@ -53,7 +28,6 @@ procedure p7 is
 		I : Integer := 0;
 		F : Duration := 0.0;
 	begin
-		delay until Start_Time;
 		loop
 			Put_Line("Low executing");
 			for J in 1 .. 10000000 loop
@@ -68,4 +42,3 @@ procedure p7 is
 begin
 	null;
 end p7;
-
